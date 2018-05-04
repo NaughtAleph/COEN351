82996a737850b1c8b6e5805e3764c4d08d021d33
NaughtAleph <addisonfattor@yahoo.com>
2018-05-04 18:54:14 +0000

    added slight error checking to checkout

--- checkout.js
+++ checkout.js
@@ -2,7 +2,7 @@
 // by the "id" parameter
 function OnlyNumbers(id) {
 	return function() {
-		console.log($(id).val());
+		//console.log($(id).val());
 		var num = $(id).val();
 		$(id).val(num.replace(/\D/g,''));
 	}
@@ -10,10 +10,4 @@ function OnlyNumbers(id) {
 $(window).on("load", function() {
 	$("#cc").bind("input",OnlyNumbers("#cc"));
 	$("#cvc").bind("input",OnlyNumbers("#cvc"));
-	for (var i=1; i<=12; i++) {
-		$("#expireMonth").append("<option value="+i+">"+(i<10?"0"+i:i)+"</option>");;
-	}
-	for (var i=2017; i<=2040; i++) {
-		$("#expireYear").append("<option value="+i+">"+i+"</option>");
-	}
 });
--- checkout.php
+++ checkout.php
@@ -1,23 +1,73 @@
+<?php
+$cc = "";
+$name = "";
+$month = 0;
+$year = 0;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$cc = $_POST["cc"];
+	$name = $_POST["name"];
+	$month = $_POST["expireMonth"];
+	$year = $_POST["expireYear"];
+	$cvc = $_POST["cvc"];
+	//echo $cc;
+	//die(mysqli_real_escape_string($cc));
+}
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Pay for shit</title>
+		<link rel="stylesheet" href="checkout.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="checkout.js"></script>
 	</head>
 	<body>
-		<!--Get items from cart-->
-		<form type="POST" action="checkout.php">
-			<input id="cc" name="cc" type="text" placeholder="Credit Card Number" maxlength=16>
+		<!--TODO: Get items from cart-->
+		<form method="post" action="checkout.php">
+<!-- possibly remove feature of remembering cc number and name, could be insecure -->
+			<input id="cc" name="cc" type="text"
+				placeholder="Credit Card Number" maxlength=16
+				<?php if ($cc != "") echo "value='$cc'"; ?> 
+			>
+			<?php
+				if ($cc && !is_numeric($cc))
+					echo "<span class='error'>".
+							"Make sure your credit card only contains numbers".
+						"</span>";
+			?>
 			<br>
-			<input id="name" name="name" type="text" placeholder="Cardholder Name">
+			<input id="name" name="name" type="text"
+				placeholder="Cardholder Name"
+				<?php if ($name != "") echo "value='$name'";?>
+			>
 			<br>
-			<!-- change to select tag (dropdown) -->
-			<select id="expireMonth" name="expireMonth"></select>
-			<select id="expireYear" name="expireYear"></select>
-			<!--<input id="expire" name="expire" type="text" placeholder="Expiration Date">-->
+			<select id="expireMonth" name="expireMonth">
+				<?php
+					for ($i = 1; $i <= 12; $i++) {
+						echo "<option value=$i".
+							($month == $i? " selected='selected' ":"")
+							.">".($i<10?"0$i":$i)."</option>";
+					}
+				?>
+			</select>
+			<select id="expireYear" name="expireYear">
+				<?php
+					for ($i = 2017; $i <= 2040; $i++) {
+						echo "<option value=$i".
+							($year == $i? " selected='selected' ":"")
+							.">$i</option>";
+					}
+				?>
+			</select>
 			<br>
 			<input id="cvc" name="cvc" type="text" placeholder="CVC" maxlength=3>
+			<?php
+				if ($cvc && !is_numeric($cvc))
+					echo "<span class='error'>".
+							"Make sure your cvc only contains numbers".
+						"</span>";
+			?>
 			<br>
 			<input type="submit" value="Submit">
 		</form>
