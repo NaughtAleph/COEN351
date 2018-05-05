03dd76c156907931b4d2aea93b54c3efaf675e0b
shubham13p <38335606+shubham13p@users.noreply.github.com>
2018-05-05 00:14:30 -0700

    Update forgot.php

--- Project/forgot.php
+++ Project/forgot.php
@@ -20,8 +20,8 @@
 require('db.php');
 session_start();
 
-if (isset($POST['username'])){
-    
+if (isset($_REQUEST['username']))
+{
     $email = stripslashes($_REQUEST['email']);
     $email = mysqli_real_escape_string($con,$email); 
     
@@ -30,11 +30,11 @@ if (isset($POST['username'])){
     
     $newpassword = stripslashes($_REQUEST['newpassword']);
     $newpassword = mysqli_real_escape_string($con,$newpassword); 
-        echo "$email";
+
     $passpattern = '/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&*()]).{4,}/';
     $emailpattern = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
 
-    if(!preg_match($passpattern, $password))
+    if(!preg_match($passpattern, $newpassword))
     {
         echo "<div class='container my-container'> 
         <form method='POST'>
@@ -53,40 +53,36 @@ if (isset($POST['username'])){
         <br/>Click here to again try<a href='forgot.php'>Back</a></div>";
     }
     
-    else {
-            $hashpass = md5($newpassword);
-            $query = "UPDATE project SET password = '$hashpass' where email='$email' and username='$username'";
-            $result = mysqli_query($con,$query);
-            $rows = mysqli_num_rows($result);
-        //    echo "$result";
+    else 
+    {
+        $hashpass = md5($newpassword);
+        $query = "UPDATE project SET password = '$hashpass' where email='$email' and username='$username'";
+        $result = mysqli_query($con,$query);
+        $rows = mysqli_num_rows($result);
 
-            if($rows==1){
-
+          if($rows>0)
+          {
+          echo "<div class='container my-container'> 
+          <form method='POST'>
+          <hr><h5><strong><em><u>ERROR</u></h5><hr>
+          <h3>Your Password was not updated successfully.</h3>
+          <h4> username or email not found </h4>
+          <br/>Click here to try again <a href='forgot.php'>Login</a></div>";            
+          }
+          else
+          {
           $_SESSION['username'] = $username;
-            // Redirect user to index.php
-        echo "<script> location.href='login.php'; </script>";
-        exit; 
+          echo "<script> location.href='login.php'; </script>";
+          exit; 
+          }
     }
-      else
-      {
-        echo "<div class='container my-container'> 
-        <form method='POST'>
-        <hr><h5><strong><em><u>ERROR</u></h5><hr>
-                    <h3>Your Password was not updated successfully.</h3>
-                    <h4> username or email not found </h4>
-                    <br/>Click here to try again <a href='forgot.php'>Login</a></div>";
-                        }
-            }
-
-}
-    else{
+}else{
 ?>
 <div class="form">
-    
+   <div class="container my-container">
 <form method="POST" action="">
-	<div class="container my-container">
-  <div class="form-group">
-    <fieldset class ="form-group">  
+	
+      
        	<hr><h5><strong><em><u>Forgot Password</u></h5><hr>
 
     	<label for="email">Email</label>
@@ -96,21 +92,20 @@ if (isset($POST['username'])){
         <br>
 
         <label for="username">Username</label>
-        <input type="text" class="form-control mx-sm-3" id="username" name="username" aria-describedby="usernamehelp" 
+        <input type="text" class="form-control mx-sm-3" id="username" name="username" aria-describedby="usernamehelp"
         placeholder="Enter Username">
         <small id="usernamehelp" class="form-text text-muted">Enter your username.</small>
         <br>
 
-    	<label for="newpassword">New Password</label>
+    	  <label for="newpassword">New Password</label>
         <input type="text" class="form-control" id="newpassword" aria-describedby="newpassHelp" name="newpassword" 
         placeholder="Enter New Password">
         <small id="newpassHelp" class="form-text text-muted">Enter your new password.</small>
         <br>
-    </fieldset>
-   </div>  
+
   <input type="submit" name="submit" value="Update">
-</div></div>
-</form>
+
+</form></div></div>
 <?php } ?>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
