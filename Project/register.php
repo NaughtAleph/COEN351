74218f20075e006e0782ea195247965bd0839f14
NaughtAleph <addisonfattor@yahoo.com>
2018-05-06 23:04:01 +0000

    changed some indentation

--- Project/register.php
+++ Project/register.php
@@ -130,54 +130,53 @@ if (isset($_REQUEST['username'])){
 ?>
 <form name="myform" method="post" action="">
 	<div class="container my-container">
-  <div class="form-group">
-  <fieldset class ="form-group">  
-   	<hr><h5><strong><em><u>GENERAL</u></h5><hr>
+		<div class="form-group">
+			<fieldset class ="form-group">  
+				<hr><h5><strong><em><u>GENERAL</u></h5><hr>
 
-	<label for="firstname">First name</label>
-    <input type="text" class="form-control mx-sm-3" id="firstname" name="firstname" aria-describedby="firstHelp" 
-    placeholder="Enter First Name" required="true">
-    <small id="firstHelp" class="form-text text-muted">Enter your first name.</small>
-    <br>
+				<label for="firstname">First name</label>
+				<input type="text" class="form-control mx-sm-3" id="firstname" name="firstname" aria-describedby="firstHelp" 
+    				placeholder="Enter First Name" required="true">
+				<small id="firstHelp" class="form-text text-muted">Enter your first name.</small>
+				<br>
 
-	<label for="lastname">Last Name</label>
-    <input type="text" class="form-control" id="lastname" aria-describedby="lastHelp" name="lastname"
-    placeholder="Enter Last Name" required="true">
-    <small id="lastHelp" class="form-text text-muted">Enter your last name.</small>
-    <br>
+				<label for="lastname">Last Name</label>
+				<input type="text" class="form-control" id="lastname" aria-describedby="lastHelp" name="lastname"
+					placeholder="Enter Last Name" required="true">
+				<small id="lastHelp" class="form-text text-muted">Enter your last name.</small>
+				<br>
 
-    <label for="contactnumber">Contact</label>
-    <input type="number" class="form-control" id="contactnumber" aria-describedby="contactHelp" name="contactnumber" placeholder="Enter Contact" required="true">
-    <small id="contactHelp" class="form-text text-muted">Enter your contact number mobile only.</small>
-    <br>
+				<label for="contactnumber">Contact</label>
+				<input type="number" class="form-control" id="contactnumber" aria-describedby="contactHelp" name="contactnumber" placeholder="Enter Contact" required="true">
+				<small id="contactHelp" class="form-text text-muted">Enter your contact number mobile only.</small>
+				<br>
 
-    <label for="exampleInputEmail">Email address</label>
-    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email" required="true">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-</fieldset>
-<fieldset>
-	<hr><h5><strong><em><u>USERNAME AND PASSWORD</u></h5><hr>
-    
-	<label for="username">Username</label>
-    <input type="text" class="form-control" id="username" placeholder="username" name="username" aria-describedby="usernameHint" required="true">
-	<small id="usernameHint" class="form-text text-muted">Please enter 8 character username.</small>
-    <br>
+				<label for="exampleInputEmail">Email address</label>
+				<input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email" required="true">
+				<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+			</fieldset>
+			<fieldset>
+				<hr><h5><strong><em><u>USERNAME AND PASSWORD</u></h5><hr>
 
-    <label for="password">Password</label>
-    <input type="text" class="form-control" id="password" placeholder="Password" name="password" aria-describedby="passwordHint" required="true">
-	<small id="passwordHint" class="form-text text-muted">Please enter strong password.</small>
-    <br>
+				<label for="username">Username</label>
+				<input type="text" class="form-control" id="username" placeholder="username" name="username" aria-describedby="usernameHint" required="true">
+				<small id="usernameHint" class="form-text text-muted">Please enter 8 character username.</small>
+				<br>
 
+				<label for="password">Password</label>
+				<input type="text" class="form-control" id="password" placeholder="Password" name="password" aria-describedby="passwordHint" required="true">
+				<small id="passwordHint" class="form-text text-muted">Please enter strong password.</small>
+				<br>
 
-    <label for="cnfpassword">Confirm Password</label>
-    <input type="password" class="form-control" name="cnfpassword" id="cnfpassword" placeholder="Password" aria-describedby="cnfpasswordHint" required="true">
-	<small id="cnfpasswordHint" class="form-text text-muted">Please enter same password as above</small>
-    <br>
+				<label for="cnfpassword">Confirm Password</label>
+				<input type="password" class="form-control" name="cnfpassword" id="cnfpassword" placeholder="Password" aria-describedby="cnfpasswordHint" required="true">
+				<small id="cnfpasswordHint" class="form-text text-muted">Please enter same password as above</small>
+				<br>
 
-    </fieldset>
-   </div>  
-  <input type="submit" name="submit" value="SAVE">
-</div>
+			</fieldset>
+		</div>  
+		<input type="submit" name="submit" value="SAVE">
+	</div>
 </form>
 <?php } ?>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
--- checkout.php
+++ checkout.php
@@ -16,60 +16,61 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Pay for shit</title>
-		<link rel="stylesheet" href="checkout.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="checkout.js"></script>
-	</head>
-	<body>
-		<!--TODO: Get items from cart-->
-		<form method="post" action="checkout.php">
+<head>
+	<title>Pay for shit</title>
+	<link rel="stylesheet" href="checkout.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+	<script src="checkout.js"></script>
+</head>
+<body>
+	<!--TODO: Get items from cart-->
+	<form method="post" action="checkout.php">
 <!-- possibly remove feature of remembering cc number and name, could be insecure -->
-			<input id="cc" name="cc" type="text"
-				placeholder="Credit Card Number" maxlength=16
-				<?php if ($cc != "") echo "value='$cc'"; ?> 
-			>
+		<input id="cc" name="cc" type="text"
+			placeholder="Credit Card Number" maxlength=16
+			<?php if ($cc != "") echo "value='$cc'"; ?> 
+		>
+		<?php
+			if ($cc && !is_numeric($cc))
+				echo "<span class='error'>".
+						"Make sure your credit card only contains numbers".
+					"</span>";
+		?>
+		<br>
+		<input id="name" name="name" type="text"
+			placeholder="Cardholder Name"
+			<?php if ($name != "") echo "value='$name'";?>
+		>
+		<br>
+		<select id="expireMonth" name="expireMonth">
 			<?php
-				if ($cc && !is_numeric($cc))
-					echo "<span class='error'>".
-							"Make sure your credit card only contains numbers".
-						"</span>";
+				for ($i = 1; $i <= 12; $i++) {
+					echo "<option value=$i".
+						($month == $i? " selected='selected' ":"")
+						.">".($i<10?"0$i":$i)."</option>";
+				}
 			?>
-			<br>
-			<input id="name" name="name" type="text"
-				placeholder="Cardholder Name"
-				<?php if ($name != "") echo "value='$name'";?>
-			>
-			<br>
-			<select id="expireMonth" name="expireMonth">
-				<?php
-					for ($i = 1; $i <= 12; $i++) {
-						echo "<option value=$i".
-							($month == $i? " selected='selected' ":"")
-							.">".($i<10?"0$i":$i)."</option>";
-					}
-				?>
-			</select>
-			<select id="expireYear" name="expireYear">
-				<?php
-					for ($i = 2017; $i <= 2040; $i++) {
-						echo "<option value=$i".
-							($year == $i? " selected='selected' ":"")
-							.">$i</option>";
-					}
-				?>
-			</select>
-			<br>
-			<input id="cvc" name="cvc" type="text" placeholder="CVC" maxlength=3>
+		</select>
+		<select id="expireYear" name="expireYear">
 			<?php
-				if ($cvc && !is_numeric($cvc))
-					echo "<span class='error'>".
-							"Make sure your cvc only contains numbers".
-						"</span>";
+				for ($i = 2017; $i <= 2040; $i++) {
+					echo "<option value=$i".
+						($year == $i? " selected='selected' ":"")
+						.">$i</option>";
+				}
 			?>
-			<br>
-			<input type="submit" value="Submit">
-		</form>
-	</body>
+		</select>
+		<br>
+		<input id="cvc" name="cvc" type="text" placeholder="CVC" maxlength=3>
+		<?php
+			if ($cvc && !is_numeric($cvc))
+				echo "<span class='error'>".
+						"Make sure your cvc only contains numbers".
+					"</span>";
+		?>
+		<br>
+		<input type="submit" value="Submit">
+	</form>
+</body>
 </html>
