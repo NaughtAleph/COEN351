c152a7bc6b9792a09a835ddfb7190e596139e631
NaughtAleph <addisonfattor@yahoo.com>
2018-05-13 01:55:57 +0000

    Added "required field" checking

--- Project/checkout.php
+++ Project/checkout.php
@@ -1,4 +1,6 @@
 <?php
+//TODO bootstrap form look
+// TODO add minus button next to things to delete them from your cart
 require("auth.php");
 require("db.php");
 $cc = "";
@@ -11,13 +13,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$month = $_POST["expireMonth"];
 	$year = $_POST["expireYear"];
 	$cvc = $_POST["cvc"];
+	if ($cc && is_numeric($cc) &&
+		$name != "" &&
+		$month && is_numeric($month) &&
+		$year && is_numeric($year) &&
+		$cvc && is_numeric($cvc)) {
+		?>
+			<html>
+			<head>
+				<meta http-equiv="refresh" content="3;./mainpage.php" />
+			</head>
+			<body>
+				<h3>Purchase(s) Successful</h3>
+				<h3>Returning you to main page in 3 seconds</h3>
+			</body>
+			</html>
+		<?php
+		//TODO clear cart cookie
+		exit;
+	}
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Pay for shit</title>
+	<title></title>
 	<link rel="stylesheet" href="css/checkout.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
@@ -37,7 +58,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				$result = mysqli_query($con, "SELECT * FROM Books WHERE id=$id");
 				while($row = mysqli_fetch_array($result)) {
 					?>
-						<tr><?php echo "<td>$row[title]</td><td>$num</td><td>$$row[price]</td><td>".sprintf("$%.2f",$num * $row["price"])."</td>"?></tr>
+						<tr>
+							<td><?php echo $row["title"] ?></td>
+							<td><?php echo $num ?></td>
+							<td>$<?php echo $row["price"] ?></td>
+							<td><?php echo sprintf("$%.2f",$num * $row["price"]) ?> </td>
+						</tr>
 					<?php
 				}
 			}
@@ -50,16 +76,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			<?php if ($cc != "") echo "value='$cc'"; ?> 
 		>
 		<?php
-			if ($cc && !is_numeric($cc))
-				echo "<span class='error'>".
-						"Make sure your credit card only contains numbers".
-					"</span>";
-		?>
+			if ($cc && !is_numeric($cc)) { ?>
+				<span class='error'>
+					Make sure your credit card only contains numbers
+				</span>
+		<?php } else if (isset($_POST["cc"]) && !$cc) { ?>
+				<span class='error'>
+					Required Field
+				</span>
+		<?php } ?>
 		<br>
 		<input id="name" name="name" type="text"
 			placeholder="Cardholder Name"
-			<?php if ($name != "") echo "value='$name'";?>
+			<?php if ($name != "") echo "value='$name'";//TODO gotta escape html?>
 		>
+		<?php if (isset($_POST["name"]) && !$name) { ?>
+			<span class='error'>
+				Required Field
+			</span>
+		<?php } ?>
 		<br>
 		<select id="expireMonth" name="expireMonth">
 			<?php
@@ -79,14 +114,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				}
 			?>
 		</select>
+		<?php
+			if ((isset($_POST["expireMonth"]) && !$month) || (isset($_POST["expireYear"]) && !$year)) { ?>
+				<span class='error'>
+					Required Field
+				</span>
+		<?php }	?>
 		<br>
 		<input id="cvc" name="cvc" type="text" placeholder="CVC" maxlength=3>
 		<?php
-			if ($cvc && !is_numeric($cvc))
-				echo "<span class='error'>".
-						"Make sure your cvc only contains numbers".
-					"</span>";
-		?>
+			if ($cvc && !is_numeric($cvc)) { ?>
+				<span class='error'>
+					Make sure your cvc only contains numbers
+				</span>
+		<?php } else if (isset($_POST["cvc"]) && !$cvc) { ?>
+				<span class='error'>
+					Required Field
+				</span>
+		<?php } ?>
 		<br>
 		<input type="submit" value="Submit">
 	</form>
